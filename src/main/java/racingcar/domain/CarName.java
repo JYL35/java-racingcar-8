45c890a620c35bd55a5dcfbe4f1fe6acd793d008
JYL35 <dlwodbs1223@naver.com>
2025-10-24 22:45:48 +0900

    refactor: CarName의 유효성 검사 로직을 분리
    CarName의 validate() 메소드가 두 가지 책임을 가지고 있어서 분리
    - validateNotNullAndBlank() : null 및 공백 검사
    - validateLength() : 이름 길이 검사

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -5,7 +5,8 @@ public class CarName {
     private final String name;
 
     public CarName(String name) {
-        validate(name);
+        validateNotNullAndBlank(name);
+        validateLength(name);
         this.name = name;
     }
 
@@ -13,11 +14,13 @@ public class CarName {
         return name;
     }
 
-    private void validate(String name) {
+    private void validateNotNullAndBlank(String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("자동차 이름은 공백이거나 비어있을 수 없습니다.");
+            throw new IllegalArgumentException("라운드 수는 공백이거나 비어있을 수 없습니다.");
         }
+    }
 
+    private void validateLength(String name) {
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 5글자 이상일 수 없습니다.");
         }
