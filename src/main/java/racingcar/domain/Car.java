fb1e1e775173f4474c860590cf525b6e9feb0e79
JYL35 <dlwodbs1223@naver.com>
2025-10-25 17:46:50 +0900

    feat: Car 클래스 구현
    Car 도메인 객체를 구현하여 TDD Red 단계에서 작성한 테스트를 통과
    - getName() : 자동차 이름(불변)을 반환
    - getDistanceMoved() : 자동차 위치를 이용하여 이동 거리를 반환

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+public class Car {
+    private final CarName carName;
+    private int position;
+
+    public Car(CarName carName) {
+        this.carName = carName;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return carName.getName();
+    }
+
+    public String getDistanceMoved() {
+        return "-".repeat(position);
+    }
+}
