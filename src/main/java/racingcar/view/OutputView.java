141d8cc11d1145876dd15d40648441decf0958f7
JYL35 <dlwodbs1223@naver.com>
2025-10-27 01:35:37 +0900

    refactor: OutputView 출력 책임 변경 및 상수화
    OutputView의 책임과 코드 구조 개선
    - 모든 라운드를 한번에 출력하던 메소드를 단일 라운드 출력으로 책임 변경
    - 헤더 메시지 출력을 담당하는 메소드 구현
    - 출력 문자열을 상수로 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,28 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
 import java.util.List;
 
 public class OutputView {
+    private static final String HEADER_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
-    public void printAllRoundResult(String allRoundResult) {
-        System.out.println("실행 결과\n" + allRoundResult);
+    public void printHeaderMessage() {
+        System.out.println(HEADER_MESSAGE);
+    }
+
+    public void printOneRoundResult(List<Car> carList) {
+        for (Car car : carList) {
+            System.out.println(car.getName() + " : " +
+                    "-".repeat(car.getPosition()));
+        }
+        System.out.println();
     }
 
     public void printWinners(List<String> winners) {
         String result = String.join(", ", winners);
 
-        System.out.println("최종 우승자 : " + result);
+        System.out.println(WINNER_MESSAGE + result);
     }
 }
