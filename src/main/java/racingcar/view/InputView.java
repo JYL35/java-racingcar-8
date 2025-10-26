bb3db85a64f0064f698cb68605d029a64b1c7e2f
JYL35 <dlwodbs1223@naver.com>
2025-10-27 01:46:25 +0900

    refactor: InputView 문자열 상수화

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,15 +3,19 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String CAR_ROSTER_MESSAGE =
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String RACE_ROUND_MESSAGE =
+            "시도할 횟수는 몇 회인가요?";
 
     public String carRosterInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(CAR_ROSTER_MESSAGE);
 
         return Console.readLine();
     }
 
     public String raceRoundInput() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(RACE_ROUND_MESSAGE);
 
         return Console.readLine();
     }
