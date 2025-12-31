d98e7cc7989b8bc73265fff72a34f2099ef7a759
JYL35 <dlwodbs1223@naver.com>
2025-12-31 17:01:57 +0900

    feat: 라운드 진행 기능 구현

--- src/main/java/racingcar/service/RaceService.java
+++ src/main/java/racingcar/service/RaceService.java
@@ -1,14 +1,35 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import racingcar.service.domain.Car;
 import racingcar.service.domain.CarName;
 
 public class RaceService {
+    private static final int RANDOM_STARTING_NUMBER = 0;
+    private static final int RANDOM_ENDING_NUMBER = 9;
 
-    public List<Car> createCars(List<String> names) {
-        return names.stream()
+    private List<Car> carRoster;
+
+    public void createCars(List<String> names) {
+        carRoster = names.stream()
                 .map(name -> new Car(new CarName(name)))
                 .toList();
+    }
+
+    public void raceStart(int RoundNumber) {
+        for (int i = 0; i < RoundNumber; i++) {
+            oneRoundStart();
+        }
+    }
+
+    private void oneRoundStart() {
+        for (Car car : carRoster) {
+            car.move(movementStrategy());
+        }
+    }
+
+    private int movementStrategy() {
+        return Randoms.pickNumberInRange(RANDOM_STARTING_NUMBER, RANDOM_ENDING_NUMBER);
     }
 }
